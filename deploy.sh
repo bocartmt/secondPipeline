8d6114751db1509a55fe708b17057e01d43b74ee
bocartmt <46490927+bocartmt@users.noreply.github.com>
2020-05-28 07:46:16 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 ssh -tt ec2-user@54.246.174.141 '
 sudo chmod 777 /var/www/html
-sudo echo "<html><h1>hello world</h1></html>" > /var/www/html/index.html'
+scp -r -p ./index.html ec2-user@54.246.174.141:/var/www/html'
 exit
