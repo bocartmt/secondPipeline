37a2286d89f5590e36263c0dbcd3984570444cac
bocartmt <46490927+bocartmt@users.noreply.github.com>
2020-05-28 07:57:34 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,2 @@
 #! /bin/bash
-ssh -tt ec2-user@54.246.174.141 '
-sudo chmod 777 /var/www/html
-scp -r -p ./index.html ec2-user@54.246.174.141:/var/www/html'
-exit
+scp -r -p ./index.html ec2-user@54.246.174.141:/var/www/html
