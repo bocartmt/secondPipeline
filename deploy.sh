44ff05620a3c7fc80f9418643c78fc6b242553ec
bocartmt <46490927+bocartmt@users.noreply.github.com>
2020-05-29 12:08:58 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-ssh -tt ec2-user@3.249.245.22
-sudo chmod 777 /var/wwww/html
+ssh -tt ec2-user@3.249.245.22 '
+sudo chmod 777 /var/www/html
+exit'
 scp -r -p -o StrictHostKeyChecking=no ./index.html ec2-user@3.249.245.22:/var/www/html
-
